bb7992028f6db48154f360dfa988449eb980152c
mzub <marcin_zub@outlook.com>
2023-04-25 18:07:37 +0200

    Add addition exceptions for both negative

--- src/main/java/bf2/Addition.java
+++ src/main/java/bf2/Addition.java
@@ -5,19 +5,36 @@ public class Addition extends Command {
     Block right;
 
     public Addition(Block left, Block right) throws Exception {
+        StringBuilder s = new StringBuilder("Addition produced negative output:\t");
         // Ex. -9 + 5
         if (left.getValueFromCommands() < 0) {
             if (left.getValueFromCommands() * -1 > right.getValueFromCommands()) {
-                throw new Exception("", null);
+                s.append(left.getValueFromCommands());
+                s.append(" + ");
+                s.append(right.getValueFromCommands());
+
+                throw new Exception(s.toString(), null);
             }
         }
         // Ex. 5 + -9
         if (right.getValueFromCommands() < 0) {
             if (left.getValueFromCommands() < right.getValueFromCommands() * -1) {
-                throw new Exception("null", null);
+                s.append(left.getValueFromCommands());
+                s.append(" + ");
+                s.append(right.getValueFromCommands());
+
+                throw new Exception(s.toString(), null);
             }
         }
-        
+        // Ex. -6 + (-7)
+        if (left.getValueFromCommands() < 0 && right.getValueFromCommands() < 0) {
+            s.append(left.getValueFromCommands());
+            s.append(" + ");
+            s.append(right.getValueFromCommands());
+
+            throw new Exception(s.toString(), null);
+        }
+
         this.left = left;
         this.right = right;
     }
