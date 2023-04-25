d1f8fad8e74b57ac4177e436de42b28828d51c93
kubijaku <kubijaku11@gmail.com>
2023-04-25 22:08:05 +0200

    works ( almost )

--- src/main/java/bf2/AntlrToLine.java
+++ src/main/java/bf2/AntlrToLine.java
@@ -7,101 +7,70 @@ import antlr.bf2Parser;
 import java.util.*;
 
 public class AntlrToLine extends bf2BaseVisitor<Line>{
+
+    public void evaluateCommand(Command command){
+        if (command instanceof IfStatement) {
+            if (((IfStatement) command).satisfied_) {
+                for (Command blockCommand : ((IfStatement) command).blockOfCommands.commands_)
+                    evaluateCommand(blockCommand);
+            }
+        } else if (command instanceof Loop) {
+            if (((Loop) command).satisfied_ ) {
+                for (Command blockCommand : ((Loop) command).blockOfCommands.commands_)
+                {
+                    evaluateCommand(blockCommand);
+                }
+                for (Cell[] ints : Board.mainBoard) {
+                    for (Cell int_ : ints) {
+                        System.out.print(int_.getValue_() + "\t");
+                    }
+                System.out.println();
+                }
+                System.out.println("\n\n");
+                evaluateCommand(command);
+            }
+            if (((Loop) command).range_ != 0) {
+                for ( int id=0; id<((Loop) command).range_; id++)
+                {
+                    for (Command blockCommand : ((Loop) command).blockOfCommands.commands_)
+                    {
+                        evaluateCommand(blockCommand);
+                    }
+                }
+            }
+        } else if (command instanceof Number tempNum) {
+            Board.updateBoard(tempNum.value_);
+        } else if (command instanceof DirectionalMove) {
+            Board.updatePointerX(((DirectionalMove) command).changeX_);
+            Board.updatePointerY(((DirectionalMove) command).changeY_);
+        } else if (command instanceof Function f) {
+            if (Objects.equals(f.name_, "READ_AS_STRING")) {
+                BF2Frame frame = new BF2Frame();
+                frame.print_as_string();
+                frame.pack();
+                frame.setVisible(true);
+            } else if (Objects.equals(f.name_, "READ_AS_INT")) {
+                BF2Frame frame = new BF2Frame();
+                frame.print_as_int();
+                frame.pack();
+                frame.setVisible(true);
+            } else if (Objects.equals(f.name_, "READ_AS_COLORS")) {
+                BF2Frame frame = new BF2Frame();
+                frame.print_as_colors();
+                frame.pack();
+                frame.setVisible(true);
+            }
+        }
+
+    }
     @Override
     public Line visitCommandComment(bf2Parser.CommandCommentContext ctx) {
         Line line = new Line();
         AntlrToCommand commandVisitor = new AntlrToCommand();
         for (int i=0; i < ctx.getChildCount(); i++) {
             Command com = commandVisitor.visit(ctx.getChild(i));
-
-            // Adding commands from ifStatement block to command list of a given line
-
-            line.addCommand(com);
+            evaluateCommand(com);
         }
-
-        List<Command> tempList = line.commandList;
-        ListIterator<Command> commandListIterator = tempList.listIterator();
-        while ( commandListIterator.hasNext() ) {
-            System.out.println(line.commandList);
-            for (Cell[] ints : Board.mainBoard) {
-                for (Cell int_ : ints) {
-                    System.out.print(int_.getValue_() + "\t");
-                }
-                System.out.println();
-            }
-            Command com = commandListIterator.next();
-
-
-            if (com instanceof Number tempNum) {
-                Board.updateBoard(tempNum.value_);
-            } else if (com instanceof DirectionalMove) {
-                Board.updatePointerX(((DirectionalMove) com).changeX_);
-                Board.updatePointerY(((DirectionalMove) com).changeY_);
-            } else if (com instanceof Loop) {
-//                if (((Loop) com).satisfied_ ) {
-//                    for (Command blockCommand : ((Loop) com).blockOfCommands.commands_)
-//                    {
-//                        commandListIterator.add(blockCommand);
-//                        commandListIterator.previous();
-//                    }
-////                    commandListIterator.add(com);
-////                    commandListIterator.previous();
-//                }
-                if (((Loop) com).range_ != 0) {
-                    for ( int id=0; id<((Loop) com).range_; id++)
-                    {
-                        int numberOfIterations = 0;
-                        for (Command blockCommand : ((Loop) com).blockOfCommands.commands_)
-                        {
-                            commandListIterator.add(blockCommand);
-                            numberOfIterations += 1;
-                        }
-                        for ( int j=0; j<numberOfIterations; j++)
-                        {
-                            commandListIterator.previous();
-                        }
-
-                    }
-                }
-            } else if (com instanceof IfStatement) {
-                System.out.println();
-                System.out.println("JEEEEEEJ");
-                System.out.println();
-                if (((IfStatement) com).satisfied_) {
-                    System.out.println(((IfStatement) com).satisfied_);
-
-                    int numberOfIterations = 0;
-                    for (Command blockCommand : ((IfStatement) com).blockOfCommands.commands_)
-                    {
-                        commandListIterator.add(blockCommand);
-                        numberOfIterations += 1;
-                    }
-                    for ( int j=0; j<numberOfIterations; j++)
-                    {
-                        commandListIterator.previous();
-                    }
-                }
-            } else if (com instanceof Function f){
-                if (Objects.equals(f.name_, "READ_AS_STRING")){
-                    BF2Frame frame = new BF2Frame();
-                    frame.print_as_string();
-                    frame.pack();
-                    frame.setVisible(true);
-                }
-                else if (Objects.equals(f.name_, "READ_AS_INT")){
-                    BF2Frame frame = new BF2Frame();
-                    frame.print_as_int();
-                    frame.pack();
-                    frame.setVisible(true);
-                } else if (Objects.equals(f.name_, "READ_AS_COLORS")){
-                    BF2Frame frame = new BF2Frame();
-                    frame.print_as_colors();
-                    frame.pack();
-                    frame.setVisible(true);
-                }
-            }
-        }
-
         return line;
     }
 
